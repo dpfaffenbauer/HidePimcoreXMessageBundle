b26ff559398e365f3aaab641f65ad13df8d54b0c
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2021-04-23 08:46:11 +0200

    pimcore-bundle

--- HidePimcoreXMessageBundle.php
+++ HidePimcoreXMessageBundle.php
@@ -2,6 +2,17 @@
 
 namespace HidePimcoreXMessageBundle;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Pimcore\Extension\Bundle\AbstractPimcoreBundle;
 
-class HidePimcoreXMessageBundle extends Bundle {}
+class HidePimcoreXMessageBundle extends AbstractPimcoreBundle
+{
+    public function getNiceName()
+    {
+        return 'Hide PimcoreX Update Message';
+    }
+
+    public function getDescription()
+    {
+        return 'Hides the PimcoreX Update Message on the login-screens';
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Hide Pimcore X Message Bundle
 Hides the Pimcore X Update Message for Pimcore 5/6 installations
+
+Installation:
+
+```
+composer require dpfaffenbauer/hide-pimcore-x-message-bundle
+
+bin/console pimcore:bundle:enable HidePimcoreXMessageBundle
+
+```
